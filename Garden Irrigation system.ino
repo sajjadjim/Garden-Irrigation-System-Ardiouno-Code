3dd1b73c2fa0e3785edd71e4aa66f58c2f2ea788
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2025-05-06 20:43:52 +0600

    Update Garden Irrigation system.ino

--- Garden Irrigation system.ino
+++ Garden Irrigation system.ino
@@ -1,11 +1,27 @@
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C dis(0x27, 16, 2);
- 
+
 void setup() {
   Serial.begin(9600);
   dis.init();
   dis.backlight();
   dis.clear();
+
+  // Step 1: Display school name
+  dis.setCursor(0, 0);
+  dis.print("Daulatpur Pilot");
+  dis.setCursor(0, 1);
+  dis.print("Girl's School");
+  delay(3000);
+  dis.clear();
+
+  // Step 2: Display Welcome message
+  dis.setCursor(3, 0); // Centered-ish
+  dis.print("Welcome !!!");
+  delay(2000);
+  dis.clear();
+
+  // Step 3: IRRIGATION SYSTEM startup message
   pinMode(2, OUTPUT);
   digitalWrite(2, HIGH);
   delay(1000);
@@ -20,28 +36,28 @@ void setup() {
   }
   dis.clear();
 }
- 
+
 void loop() {
   int value = analogRead(A0);
   Serial.println(value);
+
+  dis.setCursor(0, 0);
   if (value > 950) {
     digitalWrite(2, LOW);
-    dis.setCursor(0, 0);
     dis.print("MOTOR IS ON ");
   } else {
     digitalWrite(2, HIGH);
-    dis.setCursor(0, 0);
     dis.print("MOTOR IS OFF");
   }
- 
+
+  dis.setCursor(0, 1);
   if (value < 300) {
-    dis.setCursor(0, 1);
     dis.print("MOISTURE : HIGH");
-  } else if (value > 300 && value < 950) {
-    dis.setCursor(0, 1);
+  } else if (value < 950) {
     dis.print("MOISTURE : MID ");
-  } else if (value > 950) {
-    dis.setCursor(0, 1);
+  } else {
     dis.print("MOISTURE : LOW ");
   }
+
+  delay(500); // slow down loop
 }
